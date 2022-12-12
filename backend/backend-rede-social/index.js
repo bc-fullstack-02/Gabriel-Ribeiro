060501fd541210dc9859154dc9652d0d9ef221f8
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-12-12 00:18:28 -0300

    fix: cors

--- backend/backend-rede-social/index.js
+++ backend/backend-rede-social/index.js
@@ -33,34 +33,9 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopolog
 app.use(express.json())
 app.use(helmet());
 app.use(morgan('common'));
+app.use(cors());
+app.options('*', cors());
 
-/* //cors
-app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-}); */
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
-/* 
-  app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'URLs to trust of allow');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    if ('OPTIONS' == req.method) {
-    res.sendStatus(200);
-    } else {
-      next();
-    }
-  }); */
 //rotas
 app.use('/security', authRoute);
 app.use('/users', Auth.private, userRoute);
