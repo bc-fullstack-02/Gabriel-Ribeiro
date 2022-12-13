84dd5b68c23e53be926b4c86541798269d6e9796
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-12-13 15:31:40 -0300

    fix: rota Follow -> put para post

--- backend/backend-rede-social/routes/profileRoute.js
+++ backend/backend-rede-social/routes/profileRoute.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/userController')
 
 router.get('/', userController.listUsers);
 router.get('/:username', userController.listUser);
-router.put('/:id/follow', userController.followUser);
+router.post('/:id/follow', userController.followUser);
 
 
 module.exports = router;
